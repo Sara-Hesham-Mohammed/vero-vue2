7db161375268e65bcfbb0bd25c549ec1be6ee388
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2025-07-08 16:27:38 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>{{ product }}</h1>
+    <p>{{ description }}</p>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">More</button>
+
+    <Component :details="details" />
+  </div>
+</template>
+
+<script>
+import Component from './Component.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Component
+  },
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Warm fuzzy socks',
+      count: 0,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral']
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
---
+++ src/Component.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="detail in details" :key="detail">{{ detail }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Component',
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
