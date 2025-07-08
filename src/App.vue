0ecb6e94c4b85e6657b9d31a3140e2872204a573
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2025-07-08 17:02:47 +0300

    emitting data to parent component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
-<template>
-  <div id="app">
-    <h1>{{ product }}</h1>
-    <p>{{ description }}</p>
-    <p>Count: {{ count }}</p>
-    <button @click="increment">More</button>
-
-    <Component :details="details" />
-  </div>
-</template>
-
 <script>
-import Component from './Component.vue'
+import Parent from './Parent.vue'
 
 export default {
   name: 'App',
   components: {
-    Component
+    Parent
   },
-  data() {
-    return {
-      product: 'Socks',
-      description: 'Warm fuzzy socks',
-      count: 0,
-      details: ['80% cotton', '20% polyester', 'Gender-neutral']
-    }
-  },
-  methods: {
-    increment() {
-      this.count++
-    }
-  }
 }
 </script>
+
+<template>
+  <div id="app">
+    <Parent />
+  </div>
+</template>
+
